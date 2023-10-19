53a7b627498c7bf740bfd6ad1e5c5bb2e20b7cfa
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-19 10:08:58 +0530

    FixWarning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import SignIn from './pages/SignIn';
-import { BrowserRouter, Routes, Route, Switch, HashRouter as Router} from 'react-router-dom';
+import { Routes, Route, HashRouter as Router} from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SignUp from './pages/SignUp';
 import Dashboard from './layout/Dashboard';
